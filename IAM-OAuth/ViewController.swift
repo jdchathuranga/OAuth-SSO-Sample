9e35cb47bb91893a97c977eeb1a34946d02cf722
Chathuranga Jayawardhana Disanayaka <jdchathuranga@gmail.com>
2018-01-16 15:19:22 +0530

    Initial Commit

---
+++ IAM-OAuth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAM-OAuth
+//
+//  Created by Chathuranga Disanayaka on 1/16/18.
+//  Copyright © 2018 WSO2 Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
