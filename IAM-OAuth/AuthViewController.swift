9b94516f628fbc0d4123f2c581e8f9e687b36a7c
Chathuranga Jayawardhana Disanayaka <jdchathuranga@gmail.com>
2018-01-16 15:29:45 +0530

    Add initial xcode project Add cocoapods

--- IAM-OAuth/AuthViewController.swift
+++ IAM-OAuth/AuthViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AuthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginWithOAuthButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-IAM-OAuth/Pods-IAM-OAuth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IAM_OAuth : NSObject
+@end
+@implementation PodsDummy_Pods_IAM_OAuth
+@end
